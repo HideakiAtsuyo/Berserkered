5eb7d8b80892a39b0c5dd36b7df419cbd4431096
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-10-09 10:48:23 +0200

    Add files via upload

---
+++ Berserker Deobfuscator/Oof.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Berserker_Deobfuscator
+{
+    public static class Oof
+    {
+        public static void FileExist(string x)
+        {
+            if (!File.Exists(x))
+            {
+                Console.WriteLine("This File Doesn't Exist !");
+                Console.ReadLine();
+                Environment.Exit(1337); // flushed lmaoooooooooooooooooooo
+            }
+        }
+    }
+}
---
+++ Berserker Deobfuscator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Berserker_Deobfuscator
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            Console.Write("Obfuscated Python File Path: ");
+            string file = Console.ReadLine().Replace("\"", string.Empty);
+            Oof.FileExist(file);
+            string Berserkered = Regex.Match(File.ReadAllText(file), "('''.*''')").ToString().Replace("'''", string.Empty);
+            string code = Properties.Settings.Default.pythonScript.Replace("|content|", Berserkered);
+            string output1 = String.Format("{0}Berserkered.py", Path.GetTempPath());
+            File.WriteAllText(output1, code);
+            Console.WriteLine("Specify your python.exe File Path: ");
+            string PIP = Console.ReadLine().Replace("\"", string.Empty);
+            Oof.FileExist(PIP);
+            ProcessStartInfo proc = new ProcessStartInfo()
+            {
+                FileName = PIP,
+                Arguments = output1,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                LoadUserProfile = true
+            };
+
+            Process process = Process.Start(proc);
+            process.Start();
+            process.WaitForExit();
+
+            File.Delete(output1);
+            File.Delete(output1.Replace("Berserkered", "Oofed"));
+            Console.WriteLine("Oofed !");
+            Console.ReadLine();
+        }
+    }
+}
